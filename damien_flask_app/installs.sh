51b7aee4f66fcb1d31312fbd420a03987b513c95
pikel-damien <18dlp4@queensu.ca>
2024-04-08 11:31:26 -0400

    Update installs.sh

--- damien_flask_app/installs.sh
+++ damien_flask_app/installs.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+./#!/bin/bash
 
 
 pip install Flask
 pip install plotly
 
-pip install openmeteo-requests
-pip install requests-cache retry-requests numpy pandas
-pip install --upgrade pandas
+pip install openmeteo-requests matplotlib
+pip install requests-cache retry-requests numpy pandas scipy
+pip install --upgrade pandas./inst
 python app.py
